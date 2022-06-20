f91332fa99d7efdf6416f262af918862d981beb4
abhi9560 <54478454+abhi9560@users.noreply.github.com>
2022-06-20 17:44:32 +0530

    Update index.go

--- index.go
+++ index.go
@@ -1,11 +1,11 @@
 package index
 
 import "fmt"
-
+import "github.com/aws/aws-lambda-go/lambda"
 
 func WrapHandler(handler interface{}) lambda.Handler {
 
-	fmt.Println("Hello from wraphandler",a)
+	fmt.Println("Hello from wraphandler")
 
 	return handler
 }

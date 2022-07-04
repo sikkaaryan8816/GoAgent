859a1b0d4d479a1ec4c85049ab0ca18309c9fc22
abhi9560 <54478454+abhi9560@users.noreply.github.com>
2022-07-04 12:23:28 +0530

    Update index.go

--- index.go
+++ index.go
@@ -2,67 +2,68 @@ package index
 
 import (
 	"fmt"
-	"plugin"
+	"context"
 )
 //import "github.com/aws/aws-lambda-go/lambda"
 
-type LambdaResponse struct {
-        PlainText   string
-        EncryptedC4 string
-        DecryptedC4 string
-        EncryptedV  string
-        DecryptedV  string
-}
-type encryptionEngine interface {
-        Encrypt(string) string
-        Decrypt() (*string, error)
-}
+
 func WrapHandler(handler interface{}) interface{} {
 
 	fmt.Println("Hello from wraphandler= %v ",handler)
-	handlercall()
+	coldStart := true
 	
-	return handler
+	return func(ctx context.Context, nil) (interface{}, error) {
+		//nolint
+		ctx = context.WithValue(ctx, "cold_start", coldStart)
+		
+		
+		result, err := callHandler(ctx,handler)
+		
+		coldStart = false
+		CurrentContext = nil
+		return result, err
+	}
 }
-func handlercall() (LambdaResponse, error) {
-        resp := LambdaResponse{}
-	fmt.Println("hello world in golang")
-        // Load Cipher plugin
-        pluginModule, err := plugin.Open("/opt/cipher.so")
-        if err != nil {
-                return resp, err
-        }
-        //Load EncryptCaesar function
-        encryptCaesarSymbol, err := pluginModule.Lookup("EncryptCaesar")
-        if err != nil {
-                return resp, err
-        }
-        //Load DecryptCaesar function
-        decryptCaesarSymbol, err := pluginModule.Lookup("DecryptCaesar")
-        if err != nil {
-                return resp, err
-        }
-        //Load VermanCipher variable
-        vermanCipherSymbol, err := pluginModule.Lookup("VermanCipher")
-        if err != nil {
-                return resp, err
-        }
+func callHandler(ctx context.Context, handler interface{}) (interface{}, error) {
+	
+	handlerType := reflect.TypeOf(handler)
 
-        //Cast encryptCaesar symbol to the correct type
-        encryptCaesarFunc := encryptCaesarSymbol.(func(int, string) string)
-        //Cast encryptCaesar symbol to the correct type
-        decryptCaesarFunc := decryptCaesarSymbol.(func(int, string) string)
-        //Cast vermanCipher symbol to the correct interface type
-        vermanCipherIf := vermanCipherSymbol.(encryptionEngine)
+	args := []reflect.Value{}
 
-        resp.PlainText = "My name is Abhimanyu kumar."
-        resp.EncryptedC4 = encryptCaesarFunc(4, resp.PlainText)
-        resp.DecryptedC4 = decryptCaesarFunc(4, resp.EncryptedC4)
-        resp.EncryptedV = vermanCipherIf.Encrypt(resp.PlainText)
-        decryptedV, err := vermanCipherIf.Decrypt()
-        if err != nil {
-                return resp, err
-        }
-        resp.DecryptedV = *decryptedV
-        return resp, nil
+	if handlerType.NumIn() == 1 {
+		// When there is only one argument, argument is either the event payload, or the context.
+		contextType := reflect.TypeOf((*context.Context)(nil)).Elem()
+		firstArgType := handlerType.In(0)
+		if firstArgType.Implements(contextType) {
+			args = []reflect.Value{reflect.ValueOf(ctx)}
+		} else {
+			args = []reflect.Value{ev.Elem()}
+
+		}
+	} else if handlerType.NumIn() == 2 {
+		// Or when there are two arguments, context is always first, followed by event payload.
+		args = []reflect.Value{reflect.ValueOf(ctx), ev.Elem()}
+	}
+
+	handlerValue := reflect.ValueOf(handler)
+	output := handlerValue.Call(args)
+
+	var response interface{}
+	var errResponse error
+
+	if len(output) > 0 {
+		// If there are any output values, the last should always be an error
+		val := output[len(output)-1].Interface()
+		if errVal, ok := val.(error); ok {
+			errResponse = errVal
+		}
+	}
+
+	if len(output) > 1 {
+		// If there is more than one output value, the first should be the response payload.
+		response = output[0].Interface()
+	}
+
+	return response, errResponse
 }
+

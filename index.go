22d1f0df96669ae5b68ac55a957d76c90afc1dab
abhi9560 <54478454+abhi9560@users.noreply.github.com>
2022-07-04 12:48:14 +0530

    Update index.go

--- index.go
+++ index.go
@@ -7,7 +7,7 @@ import (
 //import "github.com/aws/aws-lambda-go/lambda"
 
 
-func WrapHandler(handler interface{}) interface{},error {
+func WrapHandler(handler interface{}) interface{}  {
 
 	fmt.Println("Hello from wraphandler= %v ",handler)
 	
@@ -15,13 +15,13 @@ func WrapHandler(handler interface{}) interface{},error {
 	
 		
 		
-		result, err := callHandler(ctx,handler)
+		result := callHandler(ctx,handler)
 		
 		
-		return result, err
+		return result 
 	}
 }
-func callHandler(ctx context.Context, handler interface{}) (interface{}, error) {
+func callHandler(ctx context.Context, handler interface{}) interface{} {
 	
 	handlerType := reflect.TypeOf(handler)
 
@@ -46,15 +46,15 @@ func callHandler(ctx context.Context, handler interface{}) (interface{}, error) 
 	output := handlerValue.Call(args)
 
 	var response interface{}
-	var errResponse error
+	//var errResponse error
 
-	if len(output) > 0 {
+	/*if len(output) > 0 {
 		// If there are any output values, the last should always be an error
 		val := output[len(output)-1].Interface()
 		if errVal, ok := val.(error); ok {
 			errResponse = errVal
 		}
-	}
+	}*/
 
 	if len(output) > 1 {
 		// If there is more than one output value, the first should be the response payload.

7f39dbb4acf473597ff35998d607d246fe5a0f85
abhi9560 <54478454+abhi9560@users.noreply.github.com>
2022-07-29 14:00:33 +0530

    Update UDPconnection.go

--- UDPconnection.go
+++ UDPconnection.go
@@ -149,7 +149,7 @@ var err error
 
 func NewAIRecord() *aiRecord {
 	r := aiRecord{}
-	r.conn, err = net.Dial("udp", "127.0.0.1:1234")
+	r.conn, err = net.Dial("udp", "66.220.31.147:12345")
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 	}

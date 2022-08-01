4c6b2e23fc3ea9b436854c0eed1f0eed58a14ee0
abhi9560 <54478454+abhi9560@users.noreply.github.com>
2022-08-01 15:41:07 +0530

    Update index.go

--- index.go
+++ index.go
@@ -40,7 +40,8 @@ func WrapHandler(handler interface{}) interface{} {
 
 		//method_entry(bt, fqmmethodentry)
 		method_entry()
-
+	        method_exit()
+		end_business_transaction()
 		CurrentContext = ctx
 		result, err := callHandler(ctx, msg, handler)
 		/*for _, listener := range listeners {
@@ -48,9 +49,9 @@ func WrapHandler(handler interface{}) interface{} {
 		}*/
 		//method_exit(bt, fqmmethodentry, 200)
 		fmt.Println("exit begin")
-		method_exit()
+		//method_exit()
 		//end_business_transaction(bt)
-		end_business_transaction()
+		//end_business_transaction()
 		coldStart = false
 		CurrentContext = nil
 		return result, err
